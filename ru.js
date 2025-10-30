774373b95987015f8dbdd821f8882c05767ff932
Cociug Marius <114507087+marryyus@users.noreply.github.com>
2025-10-30 19:21:11 +0200

    Add files via upload

---
+++ ru.js
@@ -0,0 +1,426 @@
+window.meniuRU = {
+  gustari: [
+    {
+      nume: 'СЫРНАЯ ТАРЕЛКА',
+      pret: 250,
+      gramaj: '460г',
+      descriere: 'Камамбер, Грано Падано, Эмменталь, Датский Блю, Моцарелла, Виноград, Пармезан, Крекеры, Мёд',
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/112/112431.png', 'https://cdn-icons-png.flaticon.com/512/656/656384.png']
+    },
+    {
+      nume: 'САЛАТ С ПРОШУТТО',
+      pret: 99,
+      gramaj: '245г',
+      descriere: 'Моцарелла, прошутто, оливки, черри, салат микс, оливковое масло',
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/112/112431.png']
+    },
+    {
+      nume: 'ТАРЕЛКА СО СВЕЖИМИ ОВОЩАМИ И ОВЕЧЬИМ СЫРОМ',
+      pret: 110,
+      gramaj: '640г',
+      descriere: 'Огурцы, помидоры, сладкий перец, овечий сыр, зелёный лук, петрушка',
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/112/112431.png']
+    },
+    {
+      nume: 'МЯСНАЯ ТАРЕЛКА',
+      pret: 250,
+      gramaj: '615г',
+      descriere: 'Крестьянская грудинка, копчёная шея, домашняя колбаса, кабаносси, горчица, перец, зелень',
+      alergeni: []
+    },
+    {
+      nume: 'ПИВНАЯ ТАРЕЛКА',
+      pret: 210,
+      gramaj: '',
+      descriere: 'Острые куриные крылышки, куриные наггетсы, картофель фри',
+      alergeni: []
+    },
+    {
+      nume: 'ОСТРЫЕ КУРИНЫЕ КРЫЛЫШКИ',
+      pret: 70,
+      gramaj: '200г',
+      descriere: 'Острые хрустящие куриные крылышки, подаются с соусом',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/512/112/112431.png',
+        'https://cdn-icons-png.flaticon.com/512/9293/9293703.png',
+        'https://cdn-icons-png.flaticon.com/512/2789/2789871.png',
+        'https://cdn-icons-png.flaticon.com/512/1811/1811973.png',
+        'https://cdn-icons-png.flaticon.com/512/5951/5951069.png'
+      ]
+    },
+    {
+      nume: 'КАРТОФЕЛЬ ФРИ',
+      pret: 35,
+      gramaj: '200г',
+      descriere: 'Картофель фри с соусом',
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/9293/9293703.png']
+    }
+  ],
+  salate: [
+    {
+      nume: 'САЛАТ ЦЕЗАРЬ С КУРИЦЕЙ',
+      pret: 80,
+      gramaj: '310г',
+      descriere: 'Куриная грудка, айсберг, багет (сухарики), помидоры черри, пармезан, соус',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/512/9293/9293703.png',
+        'https://cdn-icons-png.flaticon.com/512/2789/2789871.png',
+        'https://cdn-icons-png.flaticon.com/512/112/112431.png',
+        'https://cdn-icons-png.flaticon.com/512/1811/1811973.png',
+        'https://cdn-icons-png.flaticon.com/512/811/811643.png'
+      ]
+    },
+    {
+      nume: 'САЛАТ ЦЕЗАРЬ С КРЕВЕТКАМИ',
+      pret: 90,
+      gramaj: '310г',
+      descriere: 'Креветки, айсберг, багет (сухарики), помидоры черри, пармезан, соус',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/512/9293/9293703.png',
+        'https://cdn-icons-png.flaticon.com/512/2789/2789871.png',
+        'https://cdn-icons-png.flaticon.com/512/112/112431.png',
+        'https://cdn-icons-png.flaticon.com/512/1811/1811973.png',
+        'https://cdn-icons-png.flaticon.com/512/811/811643.png',
+        'https://cdn-icons-png.flaticon.com/512/3071/3071050.png'
+      ]
+    },
+    {
+      nume: 'ГРЕЧЕСКИЙ САЛАТ',
+      pret: 65,
+      gramaj: '325г',
+      descriere: 'Огурцы, помидоры, красный лук, сладкий перец, маслины, сыр фета, салат микс',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/128/112/112431.png',
+        'https://cdn-icons-png.flaticon.com/128/5289/5289110.png'
+      ]
+    },
+    {
+      nume: 'САЛАТ С ТУНЦОМ',
+      pret: 70,
+      gramaj: '255г',
+      descriere: 'Тунец, салат микс, красный лук, помидоры черри, оливковое масло, лимонный сок',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/128/811/811643.png',
+        'https://cdn-icons-png.flaticon.com/512/2789/2789871.png',
+        'https://cdn-icons-png.flaticon.com/512/1811/1811973.png',
+        'https://cdn-icons-png.flaticon.com/128/5289/5289110.png'
+      ]
+    },
+    {
+      nume: 'ТЁПЛЫЙ САЛАТ С КУРИНОЙ ГРУДКОЙ И ОСТРЫМ СОУСОМ',
+      pret: 95,
+      gramaj: '375г',
+      descriere: 'Красный лук, цукини, перец капия, чеснок, острый перец, соус чили, зелёный лук, куриная грудка',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/512/9293/9293703.png',
+        'https://cdn-icons-png.flaticon.com/128/1811/1811973.png',
+        'https://cdn-icons-png.flaticon.com/512/112/112431.png',
+        'https://cdn-icons-png.flaticon.com/512/5951/5951069.png'
+      ]
+    },
+    {
+      nume: 'ТЁПЛЫЙ САЛАТ С СВИНОЙ ШЕЙКОЙ И ОСТРЫМ СОУСОМ',
+      pret: 105,
+      gramaj: '425г',
+      descriere: 'Красный лук, цукини, перец капия, чеснок, острый перец, соус чили, зелёный лук, свиная шейка',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/128/9293/9293703.png',
+        'https://cdn-icons-png.flaticon.com/128/1811/1811973.png',
+        'https://cdn-icons-png.flaticon.com/512/112/112431.png',
+        'https://cdn-icons-png.flaticon.com/512/5951/5951069.png'
+      ]
+    },
+    {
+      nume: 'ЖАРЕНЫЕ ГРИБЫ',
+      pret: 45,
+      gramaj: '375г',
+      descriere: 'Грибы, чеснок, лук, петрушка',
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/782/782225.png']
+    },
+    {
+      nume: 'ЗАПЕЧЁННЫЕ ОВОЩИ',
+      pret: 70,
+      gramaj: '300г',
+      descriere: 'Баклажаны, цукини, перец капия, помидоры, грибы, специи для гриля, орегано, базилик',
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/782/782225.png']
+    }
+  ],
+  paste: [
+    {
+      nume: 'ПАСТА С ГРИБАМИ',
+      pret: 75,
+      gramaj: '255г',
+      descriere: 'Грибы, сливки, паста, пармезан',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/512/782/782225.png',
+        'https://cdn-icons-png.flaticon.com/512/112/112431.png',
+        'https://cdn-icons-png.flaticon.com/512/9293/9293703.png'
+      ]
+    },
+    {
+      nume: 'ПАСТА С ТУНЦОМ',
+      pret: 80,
+      gramaj: '155г',
+      descriere: 'Тунец, оливковое масло, паста, пармезан, соль',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/512/811/811643.png',
+        'https://cdn-icons-png.flaticon.com/512/9293/9293703.png',
+        'https://cdn-icons-png.flaticon.com/512/112/112431.png'
+      ]
+    },
+    {
+      nume: 'ПАСТА КАРБОНАРА',
+      pret: 85,
+      gramaj: '195г',
+      descriere: 'Бекон, яичный желток, паста, пармезан',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/512/112/112431.png',
+        'https://cdn-icons-png.flaticon.com/512/9293/9293703.png',
+        'https://cdn-icons-png.flaticon.com/512/2789/2789871.png'
+      ]
+    }
+  ],
+  pizza: [
+    {
+      nume: 'ДЕТСКОЕ МЕНЮ',
+      pret: 55,
+      gramaj: '',
+      descriere: 'Картофель фри, куриные наггетсы',
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/9293/9293703.png']
+    },
+    {
+      nume: 'ПИЦЦА "КВАТТРО ФОРМАГГИ"',
+      pret: 100,
+      gramaj: '400г',
+      descriere: 'Тесто, томатный соус / сливки, моцарелла, дор блю, грано падано, эдам, орегано',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/512/9293/9293703.png',
+        'https://cdn-icons-png.flaticon.com/512/112/112431.png'
+      ]
+    },
+    {
+      nume: 'ПИЦЦА "ЭКСПРЕСС"',
+      pret: 100,
+      gramaj: '400г',
+      descriere: 'Тесто, куриная грудка, грибы, моцарелла, эдам, маслины, томатный соус',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/512/9293/9293703.png',
+        'https://cdn-icons-png.flaticon.com/512/112/112431.png'
+      ]
+    },
+    {
+      nume: 'ПИЦЦА "МАРГАРИТА"',
+      pret: 85,
+      gramaj: '400г',
+      descriere: 'Тесто, томатный соус, моцарелла, базилик, оливковое масло, соль, орегано',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/512/9293/9293703.png',
+        'https://cdn-icons-png.flaticon.com/512/112/112431.png'
+      ]
+    },
+    {
+      nume: 'ПИЦЦА "SAVORRA"',
+      pret: 100,
+      gramaj: '400г',
+      descriere: 'Тесто, ветчина, моцарелла, рукола, эдам, пармезан',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/512/9293/9293703.png',
+        'https://cdn-icons-png.flaticon.com/512/112/112431.png'
+      ]
+    }
+  ],
+  peste: [
+    {
+      nume: 'ДОРАДО, ЗАПЕЧЁННАЯ В ДУХОВКЕ',
+      pret: 250,
+      gramaj: '450г',
+      descriere: 'Дорадо, лимон',
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/811/811643.png']
+    },
+    {
+      nume: 'ЗАПЕЧЁННЫЙ ЛОСОСЬ',
+      pret: 250,
+      gramaj: '180г',
+      descriere: 'Запечённый лосось',
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/811/811643.png']
+    }
+  ],
+  carne: [
+    {
+      nume: 'КУРИНАЯ ГРУДКА SAVORRA',
+      pret: 140,
+      gramaj: '370г',
+      descriere: 'Куриная грудка, сыр дор блю, бри, эмменталь, сливки, морковь, стручковая фасоль, чеснок',
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/112/112431.png']
+    },
+    {
+      nume: 'МАМАЛЫГА',
+      pret: 135,
+      gramaj: '400г',
+      descriere: 'Мамалыга, свиная отбивная, овечий сыр, сметана',
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/112/112431.png']
+    },
+    {
+      nume: 'СВИНОЙ ОТБИВНОЙ КАРБОНАД',
+      pret: 140,
+      gramaj: '300г',
+      descriere: 'Свиная отбивная',
+      alergeni: []
+    },
+    {
+      nume: 'БУРГЕР С СВИНИНОЙ И ГОВЯДИНОЙ',
+      pret: 80,
+      gramaj: '300г',
+      descriere: 'Свино-говяжья котлета, красный лук, маринованные огурцы, сыр чеддер, салат айсберг, булочка, помидоры, соус для бургера',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/512/112/112431.png',
+        'https://cdn-icons-png.flaticon.com/512/9293/9293703.png',
+        'https://cdn-icons-png.flaticon.com/512/2789/2789871.png',
+        'https://cdn-icons-png.flaticon.com/512/5951/5951069.png'
+      ]
+    }
+  ],
+  supe: [
+    {
+      nume: 'КУРИНЫЙ СУП С ДОМАШНЕЙ ЛАПШОЙ',
+      pret: 50,
+      gramaj: '400мл',
+      descriere: 'Куриное мясо, домашняя лапша',
+      alergeni: []
+    },
+    {
+      nume: 'СОЛЯНКА АРОМАТНАЯ',
+      pret: 65,
+      gramaj: '400мл',
+      descriere: 'Ассорти мясных продуктов, лимон',
+      alergeni: []
+    }
+  ],
+  desert: [
+    {
+      nume: 'БАБА НЕАГРЭ',
+      pret: 60,
+      gramaj: '',
+      descriere: 'Баба Неагрэ с мороженым',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/512/112/112431.png',
+        'https://cdn-icons-png.flaticon.com/512/9293/9293703.png',
+        'https://cdn-icons-png.flaticon.com/512/2789/2789871.png'
+      ]
+    },
+    {
+      nume: 'КРУАССАН С ШОКОЛАДОМ',
+      pret: 35,
+      gramaj: '',
+      descriere: 'Круассан с шоколадной начинкой',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/512/112/112431.png',
+        'https://cdn-icons-png.flaticon.com/512/9293/9293703.png',
+        'https://cdn-icons-png.flaticon.com/512/2789/2789871.png'
+      ]
+    },
+    {
+      nume: 'ЧИЗКЕЙК',
+      pret: 55,
+      gramaj: '',
+      descriere: 'Нежный чизкейк с сыром и сливками',
+      alergeni: [
+        'https://cdn-icons-png.flaticon.com/512/112/112431.png',
+        'https://cdn-icons-png.flaticon.com/512/9293/9293703.png',
+        'https://cdn-icons-png.flaticon.com/512/2789/2789871.png'
+      ]
+    }
+  ],
+  bauturi: [
+    { nume: 'МИОНЕТТО Просекко Тревизо Брют 0.75л', pret: 350 },
+    { nume: 'Белое Игристое Вино Pierre Monte Brut 0.75л', pret: 150 },
+    { nume: 'Вино Ampre Сухое Красное (Саперави, Фетяска Нягрэ, Мерло) 0.75л', pret: 250 },
+    { nume: 'Красное Сухое Вино Ampre (Саперави, Редкий Черный Виноград, Мерло) 0.75л', pret: 250 },
+    { nume: 'Ampre Сухое Белое Вино (Виорика, Рислинг, Шардоне) 0.75л', pret: 250 },
+    { nume: 'Вино Сухое Розовое Ampre (Фетяска Нягрэ, Мерло, Пино Нуар) 0.75л', pret: 250 },
+    { nume: 'Вино Roots Белое Сладкое Vero di Moscato 0.75л', pret: 260 },
+    { nume: 'Вино Roots Сладкое Розовое Vero di Moscato 0.75л', pret: 260 },
+    { nume: 'Вино Игристое Белое Metier Брют 0.75л', pret: 400 },
+    { nume: 'Белое Игристое Вино Metier Classic Брют 0.75л', pret: 400 },
+    { nume: 'Вино Игристое Metier Brut Розовое 0.75л', pret: 400 },
+    { nume: 'Вино Игристое Белое Roots Брют Blanc de Blancs 0.75л', pret: 300 },
+    { nume: 'Вино Игристое Белое Roots Брют Blanc de Noir 0.75л', pret: 300 },
+    { nume: 'Белое Игристое Вино Roots Viorica 0.75л', pret: 210 },
+    { nume: 'Ароматное Белое Игристое Вино Roots Sweet Vero di Moscato 0.75л', pret: 190 },
+    { nume: 'Корни Сладкое Ароматное Белое Игристое Вино Vero di Moscato Rose 0.75л', pret: 190 },
+    { nume: 'Контарини Просекко Тревизо Док 0.75л', pret: 260 }
+  ],
+  tarie: [
+    { nume: 'Дивин Куза Водэ 5 лет', pret: '35/270' },
+    { nume: 'Легенда Молдовы 5/7/10 лет 50 мл', pret: '40/50/55' },
+    { nume: 'Легенда Молдовы 5/7/10 лет 0.5л', pret: '300/370/500' },
+    { nume: 'Вермут Martini Extra Dry 1л', pret: 400 },
+    { nume: 'Kahlua 0.7л', pret: '45/600' },
+    { nume: 'Malibu 0.7л', pret: '45/450' },
+    { nume: 'Cream Baileys 0.7л', pret: '45/455' },
+    { nume: 'Rom Bacardi Cartea Neagră 1л', pret: '45/650' },
+    { nume: 'Джин Gibson’s London 0.7л', pret: '30/300' },
+    { nume: 'Водка Финляндия 1л', pret: '35/500' },
+    { nume: 'Jameson 1л', pret: '50/800' },
+    { nume: 'Whisky Johnnie Walker Red Label 1л', pret: '45/700' },
+    { nume: 'Sambuca 50мл', pret: 45 },
+    { nume: 'Калараш 7 лет 0.5л', pret: '35/400' },
+    { nume: 'Wilson Whisky 3 года 1л', pret: '25/370' },
+    { nume: 'Jagermeister 1л', pret: '35/600' },
+    { nume: 'Sambuca Molinar 0.7л', pret: '45/445' },
+    { nume: 'Текила Messiano Silver 1л', pret: '40/600' },
+    { nume: 'Rom Bacardi Carta Oro 1л', pret: '45/650' },
+    { nume: 'Водка Hortita 1л', pret: '30/250' },
+    { nume: 'Jack Daniels Hon 1л', pret: '55/850' },
+    { nume: 'B52 50мл', pret: 45 },
+    { nume: 'Текила 50мл', pret: 45 }],
+
+
+  bere: [
+    { nume: 'ПИВО СТЕЛЛА 0,5Л', pret: 55 },
+    { nume: 'ПИВО КОРОНА 0,33Л', pret: 55 },
+    { nume: 'ПИВО ЭФФЕС 0,5Л', pret: 35 },
+    { nume: 'ПИВО KOZEL ТЁМНОЕ 0.5Л', pret: 37 },
+    { nume: 'ПИВО СТАРЫЙ МЕЛЬНИК 0.5Л', pret: 30 }
+  ],
+  cocktails: [
+    { nume: 'Пина Колада', pret: 65 },
+    { nume: 'Безалкогольная Пина Колада', pret: 60 },
+    { nume: 'Сангрия', pret: 65 },
+    { nume: 'Джин-тоник', pret: 65 },
+    { nume: 'Мохито', pret: 65 },
+    { nume: 'Sex on the Beach', pret: 65 },
+    { nume: 'Long Island', pret: 75 },
+    { nume: 'Margarita', pret: 65 },
+    { nume: 'Tequilla Sunrise', pret: 65 }
+  ],
+
+  limonade: [
+    { nume: 'Классический Лимонад', pret: 50 },
+    { nume: 'Цитрусовый Облепиховый Лимонад', pret: 55 },
+    { nume: 'Малиновый Лимонад', pret: 55 },
+    { nume: 'Классический Мохито', pret: 50 },
+    { nume: 'Малиновый Мохито', pret: 55 },
+    { nume: 'Кола/Фанта/Спрайт 0,25л', pret: 25 },
+    { nume: 'Сок в ассортименте 0,25л', pret: 25 },
+    { nume: 'Негазированная/Газированная Вода (0,3л / 0,75л)', pret: '25/37' },
+    { nume: 'Энергетик «Burn» 0,25л', pret: 45 }
+  ],
+  ceai: [
+    { nume: 'Эспрессо', pret: 25 },
+    { nume: 'Cappucino', pret: 30 },
+    { nume: 'Латте', pret: 35 },
+    { nume: 'Американо', pret: 25 },
+    { nume: 'Флэт Уайт', pret: 45 },
+    { nume: 'Органический Заварной Чай', pret: 45 },
+    { nume: 'Домашний чай (облепиха / облепихово-имбирный / малиново-грейпфрутовый)', pret: 70 }
+  ],
+
+
+
+
+
+
+
+
+};
