a6829e2050c43dc4f5e6f4624c151f3ac769dd8c
Cociug Marius <mariuscociug096@gmail.com>
2025-10-30 17:31:57 +0200

    Savorra

---
+++ ro.js
@@ -0,0 +1,388 @@
+window.meniuRO = {
+ gustari: [
+    {
+      nume: 'PLATOU CU CAȘCAVALURI',
+      pret: 280,
+      gramaj: '460g',
+      img: 'cascaval.webp',
+      descriere: 'Camembert, Grano Padano, Emmental, Danish Blue, Mozzarella, Struguri, Parmezan, Crakers, Miere',
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/112/112431.png', 'https://cdn-icons-png.flaticon.com/512/656/656384.png']
+    },
+    {
+      nume: 'SALATĂ CU PROSCIUTTO',
+      pret: 110, 
+      gramaj: '245g',
+      img: 'prosciutto.webp',
+      descriere: 'Mozzarela, Prosciutto, Olive, Cherry, Salată Mix, Ulei de măsline',
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/112/112431.png']
+    },
+    { 
+      nume: 'PLATOU CU LEGUME PROASPETE ȘI BRÂNZĂ DE OI', 
+      pret: 110, 
+      gramaj: '640g',
+      img: 'legume proaspete.webp',
+      descriere: 'Castraveți, Roșii, Ardei dulce, Brânză de oi, Ceapă verde, Pătrunjel', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/112/112431.png']
+    },
+    {
+      nume: 'PLATOU CU CĂRNURI',
+      pret: 280,
+      gramaj: '615g',
+      img: 'carnuri.webp',
+      descriere: 'Piept țărănesc. Ceafă afumată, Tobă naturală, Cabanossi, Muștar, Ardei, Verdeață',
+      alergeni: []
+    },
+     {
+      nume: 'PLATOU LA BERE',
+      pret: 250, 
+      gramaj:'',
+      img: '',
+      descriere: 'Aripioare de pui picante, Nuggets de pui, Cartofi pai',
+      alergeni: []
+    },
+
+    { nume: 'ARIPIOARE DE PUI PICATE', 
+      pret: 80, 
+       gramaj:'200g',
+      img: '',
+       descriere: 'Aripioare de pui picante crispy servite cu sos', 
+       alergeni: ['https://cdn-icons-png.flaticon.com/512/112/112431.png','https://cdn-icons-png.flaticon.com/512/9293/9293703.png', 'https://cdn-icons-png.flaticon.com/512/2789/2789871.png', 'https://cdn-icons-png.flaticon.com/512/1811/1811973.png', 'https://cdn-icons-png.flaticon.com/512/5951/5951069.png'] },
+       {
+      nume: 'CARTOFI PAI',
+      pret: 45, 
+       gramaj:'200g',
+      img: '',
+      descriere: 'Cartofi pai serviți cu sos',
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/9293/9293703.png']
+    }
+  ],
+
+  salate: [
+   { 
+      nume: 'SALATĂ CEZAR CU PUI',
+      pret: 80,
+      gramaj: '310g',
+      img: 'cezar pui.webp',
+      descriere: 'Piept de pui, Iceberg, Baghetă (Pesmeți), Roșii Cherry, Parmezan, Sos', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/9293/9293703.png','https://cdn-icons-png.flaticon.com/512/2789/2789871.png','https://cdn-icons-png.flaticon.com/512/112/112431.png','https://cdn-icons-png.flaticon.com/512/1811/1811973.png','https://cdn-icons-png.flaticon.com/512/811/811643.png']
+    } ,
+       { 
+      nume: 'SALATĂ CEZAR CU CREVEȚI',
+      pret: 90,
+      gramaj: '310g',
+      img: '',
+      descriere: 'Creveți, Iceberg, Baghetă (Pesmeți), Roșii Cherry, Parmezan, Sos', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/9293/9293703.png','https://cdn-icons-png.flaticon.com/512/2789/2789871.png','https://cdn-icons-png.flaticon.com/512/112/112431.png','https://cdn-icons-png.flaticon.com/512/1811/1811973.png','https://cdn-icons-png.flaticon.com/512/811/811643.png','https://cdn-icons-png.flaticon.com/512/3071/3071050.png']
+    } ,
+       { 
+      nume: 'SALATĂ GRECEASCĂ',
+      pret: 70,
+      gramaj: '325g',
+      img: 'greceasca.webp',
+      descriere: 'Castraveți, Roșii, Ceapă roșie, Ardei dulce, Măsline, Brânză feta, Mix salată', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/128/112/112431.png','https://cdn-icons-png.flaticon.com/128/5289/5289110.png']
+    } ,
+       { 
+      nume: 'SALATĂ CU TON',
+      pret: 70,
+      gramaj: '255g',
+      img: 'salata ton.webp',
+      descriere: 'Ton, Salată Mix, Ceapă roșie, Roșii Cherry, Ulei Măsline, Suc de lămâie', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/128/811/811643.png','https://cdn-icons-png.flaticon.com/512/2789/2789871.png','https://cdn-icons-png.flaticon.com/512/1811/1811973.png','https://cdn-icons-png.flaticon.com/128/5289/5289110.png']
+    } ,
+       { 
+      nume: 'SALATĂ CALDĂ CU PIEPT DE PUI SERVITĂ CU SOS PICANT',
+      pret: 100,
+      gramaj: '375g',
+      img: 'salata pui.webp',
+      descriere: 'Ceapă roșie, Zuchinni, Ardei capia, Usturoi, Ardei iute, Sos Chilli, Ceapă verde, Piept de pui', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/9293/9293703.png','https://cdn-icons-png.flaticon.com/128/1811/1811973.png', 'https://cdn-icons-png.flaticon.com/512/112/112431.png','https://cdn-icons-png.flaticon.com/512/5951/5951069.png']
+    } ,
+       { 
+      nume: 'SALATĂ CALDĂ CU CEAFĂ DE PORC SERVITĂ CU SOS PICANT',
+      pret: 120,
+      gramaj: '425g',
+      img: '',
+      descriere: 'Ceapă roșie, Zuchinni, Ardei capia, Usturoi, Ardei iute, Sos Chilli, Ceapă verde, Ceafă de porc', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/128/9293/9293703.png', 'https://cdn-icons-png.flaticon.com/128/1811/1811973.png','https://cdn-icons-png.flaticon.com/512/112/112431.png','https://cdn-icons-png.flaticon.com/512/5951/5951069.png']
+    } ,
+       { 
+      nume: 'CIUPERCI LA TIGAIE ',
+      pret: 50,
+      gramaj: '375g',
+      img: 'ciuperci.webp',
+      descriere: 'Ciuperci, Usturoi, Ceapă, Pătrunjel', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/782/782225.png']
+    } ,
+       { 
+      nume: 'LEGUME COAPTE',
+      pret: 90,
+      gramaj: '300g',
+      img: 'legume coapte.webp',
+      descriere: 'Vinete, Zuchinni, Ardei capia, Roșii, Ciuperci, Condimente la grătar/Oregano/Busuioc', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/782/782225.png']
+    } ,
+
+  ],
+
+  paste: [
+   { 
+      nume: 'PASTE CU CIUPERCI',
+      pret: 75,
+      gramaj: '255g',
+      img: '',
+      descriere: 'Ciuperci, Frișcă. Paste, Parmezan', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/782/782225.png','https://cdn-icons-png.flaticon.com/512/112/112431.png','https://cdn-icons-png.flaticon.com/512/9293/9293703.png']
+    },
+    { 
+      nume: 'PASTE CU TON', 
+      pret: 80,
+      gramaj: '155g',
+      img: '', 
+      descriere: 'Ton, Ulei de măsline, Paste, Parmezan, Sare', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/811/811643.png','https://cdn-icons-png.flaticon.com/512/9293/9293703.png','https://cdn-icons-png.flaticon.com/512/112/112431.png']
+    },
+    { 
+      nume: 'PASTE CARBONARA', 
+      pret: 90,
+      gramaj: '195g',
+      img: '', 
+      descriere: 'Bacon, Gălbenuș de ou, Paste, Parmezan', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/112/112431.png','https://cdn-icons-png.flaticon.com/512/9293/9293703.png','https://cdn-icons-png.flaticon.com/512/2789/2789871.png']
+    }
+,
+  ],
+
+  pizza: [
+     { 
+      nume: 'KIDS MENU', 
+      pret: 70,
+      gramaj: '',
+      img: '', 
+      descriere: 'Cartofi pai, Nuggets de pui', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/9293/9293703.png']
+    },
+    { 
+      nume: 'PIZZA DE CASĂ “QUATTRO FORMAGGI “', 
+      pret: 110,
+      gramaj: '400g',
+      img: '', 
+      descriere: 'Aluat, Sos de roșii/ Smântână dulce, Mozzarella, Dor Blue, Grano Padano, Edamer, Oregano', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/9293/9293703.png','https://cdn-icons-png.flaticon.com/512/112/112431.png']
+    },
+    { 
+      nume: 'PIZZA DE CASĂ “EXPRESS”', 
+      pret: 110,
+      gramaj: '400g',
+      img: '', 
+      descriere: 'Aluat, Piept de pui, Ciuperci, Mozzarella, Edamer, Masline, Sos roșu', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/9293/9293703.png','https://cdn-icons-png.flaticon.com/512/112/112431.png']
+    },
+    { 
+      nume: 'PIZZA DE CASĂ “MARGHERITA”', 
+      pret: 90,
+      gramaj: '400g',
+      img: '', 
+      descriere: 'Aluat, Sos de roșii, Mozzarella, Busuioc, Ulei de măsline extravirgin, Sare, Oregano', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/9293/9293703.png','https://cdn-icons-png.flaticon.com/512/112/112431.png']
+    },
+    { 
+      nume: 'PIZZA DE CASĂ “SAVORRA”', 
+      pret: 110,
+      gramaj: '400g',
+      img: '', 
+      descriere: 'Aluat, Șuncă, Mozzarella, Rucola, Edamer, Parmezan', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/9293/9293703.png','https://cdn-icons-png.flaticon.com/512/112/112431.png']
+    }
+
+  ],
+
+  peste: [
+    { 
+      nume: 'DORADO LA CUPTOR ', 
+      pret: 250,
+      gramaj: '450g',
+      img: '', 
+      descriere: 'Dorado, Lămâie', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/811/811643.png']
+    },
+     { 
+      nume: 'SOMON COPT', 
+      pret: 250,
+      gramaj: '180g',
+      img: '', 
+      descriere: 'Somon copt', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/811/811643.png']
+    }
+
+  ],
+
+  carne: [
+       { 
+      nume: 'PIEPT DE PUI SAVORRA', 
+      pret: 160,
+      gramaj: '370g',
+      img: 'piept pui.webp', 
+      descriere: 'Piept de pui, Cascaval Dor Blue, Bri, Emmental, Frișcă, Morcov, Păstăi, Usturoi', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/112/112431.png']
+    },
+    { 
+      nume: 'MĂMĂLIGĂ', 
+      pret: 155,
+      gramaj: '400g',
+      img: '', 
+      descriere: 'Mămăligă. Friptură de porc. Brânză de oi, Smântână', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/112/112431.png']
+    },
+     { 
+      nume: 'COTLET DE PORC', 
+      pret: 160,
+      gramaj: '300g',
+      img: 'cotlet porc.webp', 
+      descriere: 'Steak de porc', 
+      alergeni: []
+    },
+     { 
+      nume: 'BURGER CU CARNE DE PORC-VITĂ', 
+      pret: 100,
+      gramaj: '300g',
+      img: 'burger.webp', 
+      descriere: 'Carne de porc-vită, Ceapă roșie. Castraveți marinați, Cașcaval Cheddar, Salată iceberg, Chiflă, Roșii, Sos pentru burger', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/112/112431.png','https://cdn-icons-png.flaticon.com/512/9293/9293703.png','https://cdn-icons-png.flaticon.com/512/2789/2789871.png','https://cdn-icons-png.flaticon.com/512/5951/5951069.png']
+    }
+  ],
+
+  supe: [
+       { 
+      nume: 'ZEAMĂ DE PUI CU TĂIȚEI DE CASĂ', 
+      pret: 60,
+      gramaj: '400ml',
+      img: '', 
+      descriere: 'Carne de pui, tăiței de casă', 
+      alergeni: []
+    },
+    { 
+      nume: 'SOLEANCĂ AROMATĂ', 
+      pret: 70,
+      gramaj: '400ml',
+      img: 'soleanca aromata.webp', 
+      descriere: 'Mix cărnuri, lămâie', 
+      alergeni: []
+    }
+      
+  ],
+
+  desert: [
+    { 
+      nume: 'BABĂ NEAGRĂ', 
+      pret: 70,
+      gramaj: '',
+      img: '', 
+      descriere: 'Babă neagră cu înghețată', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/112/112431.png','https://cdn-icons-png.flaticon.com/512/9293/9293703.png','https://cdn-icons-png.flaticon.com/512/2789/2789871.png']
+    },
+    { 
+      nume: 'CROISSANT CU CIOCOLATĂ', 
+      pret: 40,
+      gramaj: '',
+      img: '', 
+      descriere: 'Croissant umplut cu ciocolată', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/112/112431.png','https://cdn-icons-png.flaticon.com/512/9293/9293703.png','https://cdn-icons-png.flaticon.com/512/2789/2789871.png']
+    },
+    { 
+      nume: 'CHEESECAKE', 
+      pret: 65,
+      gramaj: '',
+      img: '', 
+      descriere: '', 
+      alergeni: ['https://cdn-icons-png.flaticon.com/512/112/112431.png','https://cdn-icons-png.flaticon.com/512/9293/9293703.png','https://cdn-icons-png.flaticon.com/512/2789/2789871.png']
+    },
+    
+  ],
+
+  bauturi: [
+  
+    { nume: 'MIONETTO PROSECCO TREVISO BRUT 0.75L', pret: 350 },
+  { nume: 'PIERRE MONTE VIN SPUMANT ALB BRUT 0.75L', pret: 150 },
+  { nume: 'AMPRE VIN ROȘU SEC (SAPERAVI, FETEASCĂ NEAGRĂ, MERLO) 0.75L', pret: 250 },
+  { nume: 'AMPRE VIN ROȘU SEC (SAPERAVI, RARĂ NEAGRĂ, MERLO) 0.75L', pret: 250 },
+  { nume: 'AMPRE VIN ALB SEC (VIORICA, RIESLING, CHARDONNAY) 0.75L', pret: 250 },
+  { nume: 'AMPRE VIN ROZ SEC (FETEASCĂ NEAGRĂ, MERLOT, PINOT NOIR) 0.75L', pret: 250 },
+  { nume: 'RĂDĂCINI VIN ALB DULCE VERO DI MOSCATO 0.75L', pret: 260 },
+  { nume: 'RĂDĂCINI VIN ROZ DULCE VERO DI MOSCATO 0.75L', pret: 260 },
+  { nume: 'METIER VIN SPUMANT ALB BRUT NATUR 0.75L', pret: 400 },
+  { nume: 'METIER VIN SPUMANT ALB BRUT CLASIC 0.75L', pret: 400 },
+  { nume: 'METIER VIN SPUMANT ROZ BRUT 0.75L', pret: 400 },
+  { nume: 'RĂDĂCINI VIN SPUMANT ALB BRUT BLANC DE BLANCS 0.75L', pret: 300 },
+  { nume: 'RĂDĂCINI VIN SPUMANT ALB BRUT BLANC DE NOIR 0.75L', pret: 300 },
+  { nume: 'RĂDĂCINI VIN SPUMANT ALB BRUT VIORICA 0.75L', pret: 210 },
+  { nume: 'RĂDĂCINI VIN SPUMANT ALB DULCE AROMAT VERO DI MOSCATO 0.75L', pret: 190 },
+  { nume: 'RĂDĂCINI VIN SPUMANT ALB DULCE AROMAT VERO DI MOSCATO ROSE 0.75L', pret: 190 },
+  { nume: 'CONTARINI PROSECCO TREVISO DOC 0.75L', pret: 260 }
+
+
+  ],
+tarie: [
+  { nume: 'Легенда Молдовы 5/7/10 лет 50 мл', pret: '40/50/55' },
+  { nume: 'Легенда Молдовы 5/7/10 лет 0.5л', pret: '300/370/500' },
+  { nume: 'Вермут Martini Extra Dry 1л', pret: 400 },
+  { nume: 'Kahlua 0.7л', pret: '45/600' },
+  { nume: 'Malibu 0.7л', pret: '45/450' },
+  { nume: 'Cream Baileys 0.7л', pret: '45/455' },
+  { nume: 'Rom Bacardi Cartea Neagră 1л', pret: '45/650' },
+  { nume: 'Джин Gibson’s London 0.7л', pret: '30/300' },
+  { nume: 'Водка Финляндия 1л', pret: '35/500' },
+  { nume: 'Jameson 1л', pret: '50/800' },
+  { nume: 'Whisky Johnnie Walker Red Label 1л', pret: '45/700' },
+  { nume: 'Sambuca 50мл', pret: 45 },
+  { nume: 'Калараш 7 лет 0.5л', pret: '35/400' },
+  { nume: 'Wilson Whisky 3 года 1л', pret: '25/370' },
+  { nume: 'Jagermeister 1л', pret: '35/600' },
+  { nume: 'Sambuca Molinar 0.7л', pret: '45/445' },
+  { nume: 'Текила Messiano Silver 1л', pret: '40/600' },
+  { nume: 'Rom Bacardi Carta Oro 1л', pret: '45/650' },
+  { nume: 'Водка Hortita 1л', pret: '30/250' },
+  { nume: 'Jack Daniels Hon 1л', pret: '55/850' },
+  { nume: 'B52 50мл', pret: 45 },
+  { nume: 'Текила 50мл', pret: 45 }
+],
+
+
+bere: [
+  { nume: 'BERE STELLA 0.5L', pret: 55 },
+  { nume: 'BERE CORONA 0.5L', pret: 55 },
+  { nume: 'BERE EFFES 0.5L', pret: 35 }
+],
+cocktails: [
+  { nume: 'PINA COLADA', pret: 65 },
+  { nume: 'PINA COLADA NON-ALCOOL', pret: 60 },
+  { nume: 'SANGRIA', pret: 65 },
+  { nume: 'GIN TONIC', pret: 65 },
+  { nume: 'MOJITO', pret: 65 },
+  { nume: 'SEX ON THE BEACH', pret: 65 },
+  { nume: 'LONG ISLAND', pret: 75 },
+  { nume: 'MARGARITA', pret: 65 },
+  { nume: 'TEQUILA SUNRISE', pret: 65 }
+],
+limonade: [
+  { nume: 'LIMONADĂ CLASICĂ', pret: 50 },
+  { nume: 'LIMONADĂ CU CITRUS-CĂTINĂ', pret: 55 },
+  { nume: 'LIMONADĂ CU ZMEURĂ', pret: 55 },
+  { nume: 'MOJITO CLASIC', pret: 50 },
+  { nume: 'MOJITO CU ZMEURĂ', pret: 55 },
+  { nume: 'COLA / FANTA / SPRITE 0.25L', pret: 25 },
+  { nume: 'SUC ÎN ASORTIMENT 0.25L', pret: 25 },
+  { nume: 'APĂ PLATĂ / CARBOGAZATĂ (0.3L / 0.75L)', pret: '25/37' },
+  { nume: 'BURN 0.25L', pret: 45 }
+],
+ceai: [
+  { nume: 'ESPRESSO', pret: 25 },
+  { nume: 'CAPPUCCINO', pret: 30 },
+  { nume: 'LATTE', pret: 35 },
+  { nume: 'AMERICANO', pret: 25 },
+  { nume: 'FLAT WHITE', pret: 45 },
+  { nume: 'CEAI INFUZIE', pret: 45 },
+  { nume: 'CEAI DE CASĂ (CĂTINĂ / CĂTINĂ-GHIMBIR / ZMEURĂ-GRAPEFRUIT)', pret: 70 }
+]
+
+};
---
+++ script.js
@@ -0,0 +1,201 @@
+const header = document.getElementById('header');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) header.classList.add('scrolled');
+  else header.classList.remove('scrolled');
+});
+
+
+const meniuRO = window.meniuRO;
+const meniuRU = window.meniuRU;
+
+
+const texte = {
+  ro: {
+    heroTitle: "SAVORRA CAFFÉ",
+    heroSubtitle: "Doar la SAVORRA ai șansa să savurezi!",
+    categories: {
+      gustari: "GUSTĂRI",
+      salate: "SALATE | GARNITURI",
+      paste: "PASTE",
+      pizza: "PIZZA",
+      peste: "PEȘTE",
+      carne: "CARNE",
+      supe: "SUPE",
+      desert: "DESERTURI",
+      bauturi: "BĂUTURI",
+      tarie: "TĂRIE & LICHIOR",
+      bere: "BERE",
+      cocktails: "COCKTAILS",
+      limonade: "LIMONADE & BĂUTURI CARBOGAZOASE",
+      ceai: "CEAI & CAFEA",
+      wine: "WINE CARD"
+    }
+  },
+  ru: {
+    heroTitle: "SAVORRA CAFFÉ",
+    heroSubtitle: "Только в SAVORRA у тебя есть шанс насладиться!",
+    categories: {
+      gustari: "ЗАКУСКИ",
+      salate: "САЛАТЫ | ГАРНИРЫ",
+      paste: "ПАСТА",
+      pizza: "ПИЦЦА",
+      peste: "РЫБА",
+      carne: "МЯСО",
+      supe: "СУПЫ",
+      desert: "ДЕСЕРТЫ",
+      bauturi: "НАПИТКИ",
+      tarie: "КРЕПКИЕ НАПИТКИ",
+      bere: "ПИВО",
+      cocktails: "КОКТЕЙЛИ",
+      limonade: "ЛИМОНАДЫ И ГАЗИРОВАННЫЕ НАПИТКИ",
+      ceai: "ЧАЙ И КОФЕ",
+      wine: "ВИНА"
+    }
+  }
+};
+
+let currentLang = 'ro';
+let meniuCurent = meniuRO;
+
+// animație la scroll pentru produse
+document.addEventListener("DOMContentLoaded", () => {
+  const produse = document.querySelectorAll(".card, .bauturi-item");
+
+  function animatieProduse() {
+    const triggerHeight = window.innerHeight * 1;
+    produse.forEach(p => {
+      const top = p.getBoundingClientRect().top;
+      if (top < triggerHeight) {
+        p.classList.add("show");
+      } else {
+        p.classList.remove("show");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", animatieProduse);
+  animatieProduse(); 
+
+  produse.forEach(p => p.classList.add("produs-animat"));
+});
+
+
+
+function createCard(p) {
+  const card = document.createElement('div');
+  card.className = 'card';
+  const alergeniHTML = p.alergeni && p.alergeni.length
+    ? `<div class='allergen-group'>${p.alergeni.map(a => `<img src='${a}' alt='alergen'>`).join('')}</div>`
+    : '';
+  card.innerHTML = `
+    ${p.img ? `<img src='${p.img}' alt='${p.nume}'>` : `<div style='height:300px'></div>`}
+    ${alergeniHTML}
+    <div class='content'>
+      <div class='title'>${p.nume}</div>
+      <div class='price-row'>
+        <div class='price'>${p.pret} MDL</div>
+        ${p.gramaj ? `<div class='gramaj'>${p.gramaj}</div>` : ''}
+      </div>
+    </div>`;
+  card.addEventListener('click', () => openFullPage(p));
+  return card;
+}
+
+function render() {
+  const simpleCategories = ['bauturi', 'tarie', 'bere', 'cocktails', 'limonade', 'ceai', 'wine'];
+
+  Object.keys(meniuCurent).forEach(cat => {
+    const grid = document.getElementById('grid-' + cat);
+    if (!grid) return;
+
+    if (simpleCategories.includes(cat)) {
+      grid.innerHTML = meniuCurent[cat]
+        .map(item => `<div class='bauturi-item'><span>${item.nume}</span><span>${item.pret} MDL</span></div>`)
+        .join('');
+    } else {
+      grid.innerHTML = '';
+      meniuCurent[cat].forEach(p => grid.appendChild(createCard(p)));
+    }
+  });
+}
+
+function updateMenuLinks(lang) {
+  const links = document.querySelectorAll('.menu-links a');
+  const categories = Object.keys(texte[lang].categories);
+  links.forEach((link, index) => {
+    const cat = categories[index];
+    if (cat && texte[lang].categories[cat]) {
+      link.textContent = texte[lang].categories[cat];
+    }
+  });
+}
+
+const productPage = document.getElementById('productPage');
+const prodImg = document.getElementById('prodImg');
+const prodName = document.getElementById('prodName');
+const prodDesc = document.getElementById('prodDesc');
+const prodAlergeni = document.getElementById('prodAlergeni');
+const prodPrice = document.getElementById('prodPrice');
+document.getElementById('closeFull').onclick = () => productPage.classList.remove('open');
+
+function openFullPage(p) {
+  prodImg.src = p.img || '';
+  prodName.textContent = p.nume;
+  prodDesc.textContent = p.descriere;
+  prodPrice.innerHTML = `
+    ${p.gramaj ? `<div class='gramaj-popup'>${p.gramaj}</div>` : ''}
+    <div>${p.pret} MDL</div>`;
+  prodAlergeni.innerHTML = p.alergeni && p.alergeni.length
+    ? p.alergeni.map(a => `<img src='${a}' alt='alergen'>`).join('')
+    : '';
+  productPage.classList.add('open');
+  productPage.scrollTo(0, 0);
+}
+
+// function setLanguage(lang) {
+//   currentLang = lang;
+//   meniuCurent = lang === 'ru' ? meniuRU : meniuRO;
+
+//   document.querySelector('.hero h1').textContent = texte[lang].heroTitle;
+//   document.querySelector('.hero p').textContent = texte[lang].heroSubtitle;
+
+//   Object.keys(texte[lang].categories).forEach(cat => {
+//     const el = document.querySelector(`#${cat}`);
+//     if (el) el.textContent = texte[lang].categories[cat];
+//   });
+
+//   updateMenuLinks(lang); 
+//   render();
+// }
+function setLanguage(lang) {
+  currentLang = lang;
+  meniuCurent = lang === 'ru' ? meniuRU : meniuRO;
+
+  if (lang === 'ru') {
+    Object.keys(meniuRO).forEach(categorie => {
+      meniuRU[categorie]?.forEach((produs, index) => {
+        if (!produs.img || produs.img === '') {
+          produs.img = meniuRO[categorie][index]?.img || '';
+        }
+      });
+    });
+  }
+
+  document.querySelector('.hero h1').textContent = texte[lang].heroTitle;
+  document.querySelector('.hero p').textContent = texte[lang].heroSubtitle;
+
+  Object.keys(texte[lang].categories).forEach(cat => {
+    const el = document.querySelector(`#${cat}`);
+    if (el) el.textContent = texte[lang].categories[cat];
+  });
+
+  updateMenuLinks(lang); 
+  render();
+}
+
+
+document.getElementById('lang').addEventListener('change', e => {
+  setLanguage(e.target.value);
+});
+
+render();
